c49ebd3b7c86dac00ee639f790dfb0a2e30e4910
Brian Brown <bb.brown@mac.com>
2021-01-20 15:40:19 -0800

    ConsoleAppAssignment push

---
+++ basic_c#/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ConsoleAppAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // get user number
+            Console.WriteLine("Please provide a whole number:");
+            int faveNum = Convert.ToInt32(Console.ReadLine());
+            // time 50
+            int totalMult = faveNum * 50;
+            Console.WriteLine(faveNum + " times 50 = " + totalMult);
+            Console.ReadLine();
+            // add 25
+            int totalAdd = faveNum + 25;
+            Console.WriteLine(faveNum + " plus 25 = " + totalAdd);
+            Console.ReadLine();
+            // divide by 12.5f
+            double totalDiv = faveNum / 12.5d;
+            Console.WriteLine(faveNum + " divded by 12.5 = " + totalDiv);
+            Console.ReadLine();
+            // greater than 50?
+            bool totalGreater = faveNum > 50;
+            Console.WriteLine(faveNum + " is greater than 50? = " + totalGreater);
+            Console.ReadLine();
+            // divided by 7 with remainder
+            int totalRemainder = faveNum % 7;
+            Console.WriteLine("When you divide " + faveNum + " by 7, the remainder is " + totalRemainder + ("."));
+            Console.ReadLine();
+        }
+    }
+}
--- basic_c#/HelloWorld/HelloWorld/Program.cs
+++ basic_c#/HelloWorld/HelloWorld/Program.cs
@@ -5,7 +5,27 @@ class Program
 {
     static void Main()
     {
+        // write to console and have it stay
         Console.WriteLine("Hello, \"World!\"");
+        Console.ReadLine();
+
+        // math challenges
+        int num1 = 11;
+        int num2 = 2;
+        int totalAdd = num1 + num2;
+        Console.WriteLine(totalAdd);
+        Console.ReadLine();
+
+        int totalSub = num2 - num1;
+        Console.WriteLine(totalSub);
+        Console.ReadLine();
+
+        int totalMult = num1 * num2;
+        Console.WriteLine(totalMult);
+        Console.ReadLine();
+
+        double totalDiv = (double)num2 / num1;
+        Console.WriteLine(totalDiv);
         Console.ReadLine();
     }
 }
