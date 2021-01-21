b9aa4f3d34945d6ed719912e9b34049eb78c529c
Brian Brown <bb.brown@mac.com>
2021-01-21 14:38:58 -0800

    BranchingAssignemtn push

---
+++ basic_c#/BranchingAssignment/BranchingAssignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BranchingAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // greeting
+            Console.WriteLine("Welcome to Package Express. \nPlease folow the instructions below.");
+            Console.ReadLine();
+
+            // user input weight
+            Console.WriteLine("How much does your package weigh?");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight > 50)  // kick out if too heavy
+            {
+                Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a nice day.");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+            else
+            {
+                Console.ReadLine(); // if not then continue
+            }
+
+            // user input package dimensions
+            Console.WriteLine("Please enter your package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter your package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            if (width + height + length > 50) // kick out if it's too big
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express. Have a nice day!");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+            else // shipping calculation
+            {
+                double total = ((width * height * length) * (weight)) / 100.00;
+                Console.WriteLine("Your esitmated shipping total is: $" + total + "\nThank you!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ basic_c#/IfElseStatements/IfElseStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace IfElseStatements
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?(lowercase please)");
+            string name = Console.ReadLine();
+            if (name == "brian")
+            {
+                Console.WriteLine("Your name is " + name + ", that is a cool name!");
+                Console.ReadLine();
+            }
+            else if (name == "bryan")
+            {
+                Console.WriteLine("Your name is " + name + ", it would be cool, if you spelled it right...");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Your name is " + name + ", this is not a cool name.");
+                Console.ReadLine();
+            }
+            //int num1 = 9;
+            //int num2 = 11;
+
+            //string result = num1 < num2 ? "num1 is less than num2" : "num1 is not less than num2";
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+        }
+    }
+}
