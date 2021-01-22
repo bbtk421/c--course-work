05ca36db2308023929c8cbe92bb9491b4ea05be6
Brian Brown <bb.brown@mac.com>
2021-01-21 17:12:32 -0800

    StringFunctionsAssignemnt push

---
+++ basic_c#/StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Brian";
+            //string quote = "The man said, \"Hello\", Brian.\nHello on a new line(cinema).\n \tHello on a new tab.";
+            //string filePath = @"C:\Users\bbtk4";
+
+            //bool trueOrFalse = name.Contains("s");
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Brian");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ basic_c#/StringFunctionsAssignment/StringFunctionsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace StringFunctionsAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sentence1 = "String sentence number one. ";
+            string sentence2 = "String sentence number two. ";
+            string sentence3 = "String sentence number three. ";
+
+            sentence2 = sentence2.ToUpper(); // changing to all uppercase
+
+            Console.WriteLine(sentence1 + sentence2 + sentence3); // concats
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder(sentence1 + sentence3); // StringBuilder named sb and (args)
+
+            sb.Append("String sentence number 4."); // add to the end
+
+            sb.Insert(28, sentence2); // insert at given char number
+
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+
+        }
+    }
+}
