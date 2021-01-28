fc3b454c723063fe84fd3d079a7b3c1f42d85c88
Brian Brown <bb.brown@mac.com>
2021-01-27 16:22:19 -0800

    ExceptionHandlingAssignment push

---
+++ basic_c#/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+		bool check = false;
+		do
+		{
+			try
+			{
+				Console.WriteLine("Pick a number.");
+				int numberOne = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Pick a second number.");
+				int numberTwo = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Dividing the two...");
+				int numberThree = numberOne / numberTwo;
+				Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree + ".");
+				Console.ReadLine();
+				check = true;
+			}
+			catch (FormatException ex)
+			{
+				Console.WriteLine("Please type a whole number."); // this would need to save the new input
+			}
+			catch (DivideByZeroException ex)
+			{
+				Console.WriteLine("Please don't divide by zero..."); // this would need to save the new inpu
+			}
+			catch (Exception ex) // this would need to save the new input
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally // can be used to log exceptions
+			{
+				Console.ReadLine();
+			}
+		}
+		while (!check);
+        }
+    }
+
---
+++ basic_c#/ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandlingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = false; 
+            do // makes the program run again after an exception instead of ending
+            {
+                try
+                {
+                    List<int> numList = new List<int>() { 2, 4, 6, 8, 10 };
+                    Console.WriteLine("Please enter a whole number.");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    foreach (int i in numList)
+                    {
+                        int result = i / num1;
+                        Console.WriteLine(result);
+                    }
+                    Console.ReadLine();
+                    check = true;
+                }
+                catch (FormatException ex) // catched not an int
+                {
+                    Console.WriteLine("Please type a whole number.");
+                }
+                catch (DivideByZeroException ex) // catches zero
+                {
+                    Console.WriteLine("Please dont divide by zero...");
+                }
+                catch (Exception ex) // catches whatever else?
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+            }
+            while (!check); // allows it to end if no exceptions
+            Console.WriteLine("Try/Catch block finished.");
+            Console.ReadLine();
+        }
+    }
+}
