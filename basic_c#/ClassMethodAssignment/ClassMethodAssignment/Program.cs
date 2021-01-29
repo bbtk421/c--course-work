7557f4776857783c5806e1a5e692090df2ef6ec4
Brian Brown <bb.brown@mac.com>
2021-01-28 16:33:24 -0800

    ClassMethodAssignment push

---
+++ basic_c#/ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreeMethods methods = new ThreeMethods();
+            Console.WriteLine("Please provide a whole number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            methods.Plus2(num1);
+            Console.ReadLine();
+
+            methods.Times2(num1);
+            Console.ReadLine();
+
+            methods.Squared(num1);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ basic_c#/ClassMethodAssignment/ClassMethodAssignment/ThreeMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public class ThreeMethods
+    {
+        public int Plus2(int num1)
+        {
+            int addResult = num1 + 2;
+            Console.WriteLine(num1 + " plus 2, equals " + addResult);
+            return addResult;
+        }
+
+        public int Times2(int num1)
+        {
+            int timesResult = num1 * 2;
+            Console.WriteLine(num1 + " times two, equals " + timesResult);
+            return timesResult;
+        }
+
+        public int Squared(int num1)
+        {
+            int squareResult = num1 * num1;
+            Console.WriteLine(num1 + " times itelf, equals " + squareResult);
+            return squareResult;
+        }
+    }
+}
