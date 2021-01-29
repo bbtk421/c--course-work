f7a60a370b185c3ab3c1ae624defbd20e615ed57
Brian Brown <bb.brown@mac.com>
2021-01-28 16:53:59 -0800

    ClassMethodAssignment2 push

---
+++ basic_c#/ClassMethodAssignment2/ClassMethodAssignment2/MathMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment2
+{
+    public class MathMethods
+    {
+        public int Math(int num1)
+        {
+            int result = num1 + 2;
+            Console.WriteLine(num1 + " plus 2, equals " + result);
+            return result;
+        }
+
+        public int Math(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine(num1 + " times " + num2 + ", equals " + result);
+            return result;
+        }
+
+        public int Math(int num1, int num2, int num3)
+        {
+            int result = num1 - num2 + num3;
+            Console.WriteLine(num1 + " minus " + num2 + " plus " + num3 + ", equals " + result);
+            return result;
+        }
+    }
+}
---
+++ basic_c#/ClassMethodAssignment2/ClassMethodAssignment2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathMethods methods = new MathMethods();
+            int num2 = 4;
+            int num3 = 5;
+            Console.WriteLine("Please provide a whole number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            methods.Math(num1);
+            Console.ReadLine();
+
+            methods.Math(num1, num2);
+            Console.ReadLine();
+
+            methods.Math(num1, num2, num3);
+            Console.ReadLine();
+        }
+    }
+}
