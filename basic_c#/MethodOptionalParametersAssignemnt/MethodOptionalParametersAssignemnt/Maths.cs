e1f4aaab230af37171b72134f49e5239089207c3
Brian Brown <bb.brown@mac.com>
2021-01-29 13:46:50 -0800

    MethodOptionalParametersAssignment push

---
+++ basic_c#/MethodOptionalParametersAssignemnt/MethodOptionalParametersAssignemnt/Maths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParametersAssignemnt
+{
+    public class Maths
+    {
+        public int Math(int num1, int num2 = 1)
+        {
+            int result = (num1 + num2) * 2;
+            Console.WriteLine(num1 + " plus " + num2 + " times 2, equals " + result);
+            return result;
+        }
+    }
+}
---
+++ basic_c#/MethodOptionalParametersAssignemnt/MethodOptionalParametersAssignemnt/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOptionalParametersAssignemnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maths method = new Maths();
+
+            Console.WriteLine("Please provide a whole number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please provide another whole number(or don't, it's your life...)");
+            string num2 = Console.ReadLine();
+            if (num2 == "")
+            {
+                method.Math(num1);
+            }
+            else
+            {
+                int num3 = Convert.ToInt32(num2);
+                method.Math(num1, num3);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
