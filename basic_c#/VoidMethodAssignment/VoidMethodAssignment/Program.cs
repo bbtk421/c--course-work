479eb86e16172f49d21d193e3e6827392aaccf50
Brian Brown <bb.brown@mac.com>
2021-01-30 14:29:08 -0800

    VoidMethodAssignment push

---
+++ basic_c#/VoidMethodAssignment/VoidMethodAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            voidMath math1 = new voidMath();
+            math1.Maths(4, 5);
+            math1.Maths(num1: 3, num2: 6);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ basic_c#/VoidMethodAssignment/VoidMethodAssignment/voidMath.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodAssignment
+{
+    public class voidMath
+    {
+        public void Maths(int num1, int num2)
+        {
+            int result = num1 + 1;
+            int result2 = num2;
+
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+        }
+    }
+}
