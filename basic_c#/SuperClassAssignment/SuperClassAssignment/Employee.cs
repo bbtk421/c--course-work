3dbaa2a6e11fb39d288b4c9d9a96da594b53a19c
Brian Brown <bb.brown@mac.com>
2021-01-30 17:41:01 -0800

    SuperClassAssignment push

---
+++ basic_c#/SuperClassAssignment/SuperClassAssignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClassAssignment
+{
+    public class Employee : Person // inherit from Person class
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ basic_c#/SuperClassAssignment/SuperClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee // instanciate
+            {
+                FirstName = "Sample", // initialize
+                LastName = "Student"
+            };
+
+            employee.SayName(); // call method on object
+            Console.ReadLine();
+        }
+    }
+}
