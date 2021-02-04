60c74ca9eaa47ccb0effdfac9fdec4c71faa1769
Brian Brown <bb.brown@mac.com>
2021-02-03 18:53:40 -0800

    StructAssignment push

---
+++ basic_c#/StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Struct.Number number = new Struct.Number();
+            number.Amount = 10;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ basic_c#/StructAssignment/StructAssignment/Struct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public class Struct
+    {
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
