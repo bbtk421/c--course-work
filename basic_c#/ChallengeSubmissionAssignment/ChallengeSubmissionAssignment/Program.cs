6b089b0d9d8f5a8ac76458a69785517a022afab8
Brian Brown <bb.brown@mac.com>
2021-03-07 14:37:36 -0800

    C# ChallengeSubmissionAssigment push for help

---
+++ basic_c#/ChallengeSubmissionAssignment/ChallengeSubmissionAssignment/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Web.Mvc;
+
+
+namespace ChallengeSubmissionAssignment
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+		
+		public void OnGet()
+		{
+			string dateTime = DateTime.Now.ToShortTimeString();
+			ViewData["TimeStamp"] = dateTime;
+		}
+	}
+}
